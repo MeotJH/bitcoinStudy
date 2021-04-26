7393ed8cd5174c65213164176faa3e563107cc03
KJH <mallangyi@naver.com>
2021-04-26 22:27:59 +0900

    first commit

---
+++ exam.py
@@ -0,0 +1,13 @@
+from FieldElement import FieldElement
+
+a = FieldElement(7,13)
+b = FieldElement(12,13)
+c = FieldElement(6,13)
+
+print( a+b == c)
+
+d = FieldElement(7,13)
+e = FieldElement(2,13)
+f = FieldElement(5,13)
+
+print( d - e == f )

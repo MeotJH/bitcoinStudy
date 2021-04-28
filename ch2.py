575a788c0db5670a4c8cff961038186ec42a2e21
KJH <mallangyi@naver.com>
2021-04-28 22:34:20 +0900

    siba jonna hard

--- ch2.py
+++ ch2.py
@@ -1,6 +1,9 @@
 from ecc import Point
 
-#p1 = Point(2,4,5,7)
+
+
+p1 = Point(-1, 1, 5, 7)
 p2 = Point(-1, -1, 5, 7)
-p3 = Point(18,77, 5, 7)
-#p4 = Point(5,7, 5, 7)
+p3 = Point(None, None, 5, 7)
+
+print(p1+p2)

08c27f81682da13bc11e06c0dd0663021a6582fe
KJH <mallangyi@naver.com>
2021-05-10 22:46:57 +0900

    for a long time 3.7

--- ch2.py
+++ ch2.py
@@ -2,13 +2,11 @@ import ecc
 #from helper import run
 
 prime = 223
-a = ecc.FieldElement(0,prime)
-b = ecc.FieldElement(7,prime)
-x1 = ecc.FieldElement(192, prime)
-y1 = ecc.FieldElement(105, prime)
-x2 = ecc.FieldElement(17, prime)
-y2 = ecc.FieldElement(56, prime)
-p1 = ecc.Point(x1, y1, a, b)
-p2 = ecc.Point(x2, y2, a, b)
-
-print(p1+p2)
+a = ecc.FieldElement(0, prime)
+b = ecc.FieldElement(7, prime)
+x = ecc.FieldElement(47, prime)
+y = ecc.FieldElement(71, prime)
+p = ecc.Point(x, y, a, b)
+for s in range(1,21):
+        result = s*p
+        print('{}*(47,71)=({},{})'.format(s,result.x,result.y.num)) 
--- ecc.py
+++ ecc.py
@@ -1,16 +1,15 @@
 class FieldElement:
-    
+
     def __init__(self, num, prime):
-        if num >= prime or num < 0 :
-            error = 'num {} not in field range 0 to {}'.format(
-                num, prime -1)
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
             raise ValueError(error)
         self.num = num
         self.prime = prime
 
     def __repr__(self):
-        return 'FieldElement_{}({})'.format(self.prime, self.num)   
-        
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
 
     def __eq__(self, other):
         if other is None:
@@ -18,34 +17,57 @@ class FieldElement:
         return self.num == other.num and self.prime == other.prime
 
     def __ne__(self, other):
+        # this should be the inverse of the == operator
         return not (self == other)
 
     def __add__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num + other.num )%self.prime
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num + other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
     def __sub__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num - other.num)%self.prime
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num - other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num * other.num)%self.prime
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num * other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
     def __pow__(self, exponent):
-        n = exponent % ( self.prime-1 )
-        num = pow(self.num , n , self.prime)
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
     def __truediv__(self, other):
-        num = self.num*pow(other.num,self.prime-2,self.prime)%self.prime
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
     
 class Point:
 
@@ -95,16 +117,25 @@ class Point:
             y = inclination*(self.x - x ) - self.y
             return self.__class__( x, y, self.a, self.b)
 
-        if self.x == other.x:
-
-            inclination = (3*(self.x**2)+self.a)/(2*self.y)
-            print(inclination)
-            x = inclination**2 - 2*self.x
-            y = inclination*(self.x - x) - self.y
-            return self.__class__( x, y, self.a, self.b)
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
 
         if self == other and self.y == 0 * self.x:
             return self.__class__(None, None, self.a, self.b)
+        
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self  # <1>
+        result = self.__class__(None, None, self.a, self.b)  # <2>
+        while coef:
+            if coef & 1:  # <3>
+                result += current
+            current += current  # <4>
+            coef >>= 1  # <5>
+        return result
 
 # class ECCTest(TestCase):
 

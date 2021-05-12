620caca4d2eb5e7f560534127613d13c7935ebfc
KJH <mallangyi@naver.com>
2021-05-12 22:41:35 +0900

    3.12.3

--- ch2.py
+++ ch2.py
@@ -1,15 +1,11 @@
-from ecc import G,N
-#from helper import run
-
-# gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
-# gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
-# p = 2**256 - 2**32 - 977
-# n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-# x = ecc.FieldElement(gx, p)
-# y = ecc.FieldElement(gy, p)
-# seven = ecc.FieldElement(7, p)
-# zero = ecc.FieldElement(0, p)
-# G = ecc.Point(x, y, zero, seven)
-# print(n*G)
-
-print( N * G )
+from ecc import S256Point, G, N
+z = 0xbc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423
+r = 0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6
+s = 0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+px = 0x04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574
+py = 0x82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4
+point = S256Point(px, py)
+s_inv = pow(s, N-2, N)  # <1>
+u = z * s_inv % N  # <2>
+v = r * s_inv % N  # <3>
+print((u*G + v*point).x.num == r)  # <4>

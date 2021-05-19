cb1905441e0582a72624693a17448857da62fd49
KJH <mallangyi@naver.com>
2021-05-19 21:16:39 +0900

    5-1

--- ecc.py
+++ ecc.py
@@ -318,3 +318,36 @@ class PrivateKey:
             suffix = b''
 
         return encode_base58_checksum(prefix + secret_bytes + suffix)
+class Tx:
+
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+        self.testnet = testnet
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+
+        tx_outs = ''
+        for tx_out in tx_outs:
+            tx_outs += tx_out.__repr() + '\n'
+        
+        return 'tx: {}\nversion: {}\ntx_ins:\n{}tx_outs:\nlocktime: {}'.format(
+            self.id(),
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.locktime,
+        )
+    def id(self):
+        ''Human-readable hexadecimal of the transaction hash''
+        return self.hash().hex()
+
+    def hash(self):
+        '''Bianry hash of the lagacy serializationn'''
+        return hash256(self.serialize())[::-1]
+

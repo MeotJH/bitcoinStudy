0c77a57224f588490868bb62ed4d83a8f9ea99e3
KJH <mallangyi@naver.com>
2021-05-21 19:15:23 +0900

    5.2

--- ecc.py
+++ ecc.py
@@ -354,6 +354,23 @@ class Tx:
     @classmethod
     def parse(cls, stream):
         version = helper.little_endian_to_int(stream.read(4))
+        TxIn = 
         return cls(version, None, None, None, testnet=testnet)
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
 
 
